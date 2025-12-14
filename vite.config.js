12b5a7bbda7f78556121875f2b30686e68ebc760
ogdeo24 <ogddeo@gmail.com>
2025-12-14 00:40:22 -0500

    new new

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/naija-restaurant-react/',  // 👈 repo name here
-})
+  base: "/", // REQUIRED for Vercel
+});
