b99ba1a7ed6cdcc972fd3e4b9a394a1f82d16ded
ogdeo24 <ogddeo@gmail.com>
2025-12-14 00:12:41 -0500

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
-import { CartProvider } from "./CartContext.jsx";
+import { CartProvider } from "./CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
